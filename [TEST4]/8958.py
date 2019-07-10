ef332a4c957fa33662508f0c0a8a30f75197f21c
cdgus1514 <37209022+cdgus1514@users.noreply.github.com>
2019-07-10 17:41:44 +0900

    Add files via upload

--- [TEST4]/8958.py
+++ [TEST4]/8958.py
@@ -2,9 +2,32 @@
 # O일때, cnt++
 # X일때, cnt=0
 
+ox = []
+cnt = 0
+score = 0
+
+
 # 테스트 케이스
 T = int(input(">> "))
 
-for i in range(T):
 
-    os = list(map(int, input(">> ")))
+# 데이터 입력
+for i in range(T):
+    str = input(">> ")
+    ox.append(list(str))
+
+
+# 점수 체크
+for i in range(T):
+    cnt=0; score=0
+    for j in range(len(ox[i])):
+        if ox[i][j] == "O":
+            cnt+=1
+            score+=cnt
+        else:
+            cnt=0
+
+    print(score)
+
+
+#for i in range(len(test))
